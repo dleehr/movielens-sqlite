9f64d2257c39b84f63a82e88ed0bba45dc7e5082
Dan Leehr <dan.leehr@duke.edu>
2015-07-29 14:38:31 -0400

    Add year column

--- make_sqlite.sh
+++ make_sqlite.sh
@@ -17,5 +17,15 @@ EOF
   echo ".read import.sql" | sqlite3 movielens-small.db
   rm import.sql
 
-#   rm -rf ml-latest-small
+  rm -rf ml-latest-small
+
+cat <<EOF > addyear.sql
+create table movies_years as select movieId, title, substr(trim(title),-5,4) as year, genres from movies;
+update movies_years set year = null where year not like '1%' and year not like '2%';
+drop table movies;
+create table movies as select * from movies_years;
+drop table movies_years;
+EOF
+  echo ".read addyear.sql" | sqlite3 movielens-small.db
+  rm addyear.sql
 fi
